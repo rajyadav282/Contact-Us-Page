e09c9f548df9926fbcd37bb561c6a3d8f6b24ee3
Raj <yraj98391@gmail.com>
2025-04-21 22:32:24 +0530

    New Changes Made

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+    return (
+        <main className="hero container">
+            <div className="hero-content">
+                <h1>CONTACT US</h1>
+                <p>LET'S CONNECT : WE'RE HERE TO HELP AND WE'D LOVE TO HEAR FROM YOU WHETHER YOU HAVE A QUESTION,
+                   COMMENT OR JUST WANT TO CHAT , YOU CAN REACH TO US THROUGH THE CONTACT FORM OF THIS PAGE OR BY PHONE , EMAIL OR SOCIAL MEDIA.
+                </p>
+            </div>
+            <div className='hero-image'>
+            <img src="/images/contact.svg"/>
+            </div>
+        </main>
+    )
+}
+export default Hero;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+const Navigation = () => {
+
+    return (
+            <nav>
+            <div className="logo">
+             <img src="/public/images/logo.png"/>
+            </div>
+               
+             <ul>
+              <li>HOME</li>
+              <li>ABOUT</li>
+              <li>CONTACT</li>
+            </ul>
+            </nav>
+        
+    )
+
+}
+
+export default Navigation;
